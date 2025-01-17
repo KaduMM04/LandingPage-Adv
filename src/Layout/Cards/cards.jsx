5cd3f560ae2415acba66fea1c1e1a7ffefdc8ce0
Kadu Miranda <kadumendesmiranda@gmail.com>
2025-01-17 17:48:23 -0300

    Stylized cards with yours img and new functions

--- src/Layout/Cards/cards.jsx
+++ src/Layout/Cards/cards.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import "./cards.css";
 
+
+import TrabalhistaImg from "C:/temp/LandingPages/mendesmirandaadv/src/Layout/Imgs/CardsImgs/trabalhista.jpg";
+import CivilImg from "C:/temp/LandingPages/mendesmirandaadv/src/Layout/Imgs/CardsImgs/civil.jpg";
+import PrevImg from "C:/temp/LandingPages/mendesmirandaadv/src/Layout/Imgs/CardsImgs/prev.webp";
+
 const Cards = () => {
   const [flippedCards, setFlippedCards] = useState({});
 
@@ -16,16 +21,19 @@ const Cards = () => {
       title: "Direito Trabalhista",
       frontText: "Direito Trabalhista",
       backText: "Proteção dos direitos dos trabalhadores e empregadores.",
+      backgroundImage: TrabalhistaImg,
     },
     {
       title: "Direito Civil",
       frontText: "Direito Civil",
       backText: "Soluções para contratos, família e outros casos civis.",
+      backgroundImage: CivilImg,
     },
     {
       title: "Direito Previdenciário",
       frontText: "Direito Previdenciário",
       backText: "Questões previdenciárias como aposentadoria, pensões e auxílios.",
+      backgroundImage: PrevImg,
     },
   ];
 
@@ -38,9 +46,13 @@ const Cards = () => {
             key={index}
             className={`card ${flippedCards[index] ? "flipped" : ""}`}
             onClick={() => toggleFlip(index)}
+            style={{
+              backgroundImage: `url(${area.backgroundImage})`,
+            }}
           >
             <div className="card-front">
               <h3>{area.frontText}</h3>
+              <div className="notification">Clique para mais detalhes</div>
             </div>
             <div className="card-back">
               <p>{area.backText}</p>
